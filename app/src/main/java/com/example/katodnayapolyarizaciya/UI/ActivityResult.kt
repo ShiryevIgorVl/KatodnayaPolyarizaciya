610a4586317c42a493d556340ca7240936b05f93
Igor <ishirayev@gmail.com>
2021-03-31 20:48:51 +0300

    Готово. Надо навести красоту

--- app/src/main/java/com/example/katodnayapolyarizaciya/UI/ActivityResult.kt
+++ app/src/main/java/com/example/katodnayapolyarizaciya/UI/ActivityResult.kt
@@ -1,15 +1,13 @@
 package com.example.katodnayapolyarizaciya.UI
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.katodnayapolyarizaciya.Constans.Constans
-import com.example.katodnayapolyarizaciya.R
 import com.example.katodnayapolyarizaciya.databinding.ActivityResultBinding
 
 class ActivityResult : AppCompatActivity() {
     private lateinit var binding: ActivityResultBinding
-    private val constans = Constans
+    private val constants = Constans
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,9 +15,9 @@ class ActivityResult : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-        binding.corrent.text = constans.I + intent.getStringExtra(constans.I)
-        binding.resistanceR.text = constans.RR + intent.getStringExtra(constans.RR)
-        binding.resistanceT.text = constans.RT + intent.getStringExtra(constans.RT)
-        binding.Utz.text = constans.UTZ + intent.getStringExtra(constans.UTZ)
+        binding.corrent.text = constants.I + intent.getStringExtra(constants.I)
+        binding.resistanceR.text = constants.RR + intent.getStringExtra(constants.RR)
+        binding.resistanceT.text = constants.RT + intent.getStringExtra(constants.RT)
+        binding.Utz.text = constants.UTZ + intent.getStringExtra(constants.UTZ)
     }
 }
